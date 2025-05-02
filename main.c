eced90d8a57b711a1c1dc9e42494ccb3c8369e5b
adept3101 <82520598+adept3101@users.noreply.github.com>
2025-05-02 14:43:40 +0300

    main file

--- main.c
+++ main.c
@@ -2,13 +2,68 @@
 #include <stdlib.h>
 #include <string.h>
 #include "curl/include/curl/curl.h"
-//#include <curl/curl.h>
 #include "cJSON/cJSON.h"
 
 struct memory {
     char *response;
     size_t size;
 };
+
+typedef struct {
+    const char *dir;
+    const char *arrow;
+} WindDirMap;
+
+WindDirMap wind_map[] = {
+    {"N",   "↑"},
+    {"NNW", "↖"},
+    {"NW",  "↖"},
+    {"WNW", "↖"},
+    {"W",   "←"},
+    {"WSW", "↙"},
+    {"SW",  "↙"},
+    {"SSW", "↙"},
+    {"S",   "↓"},
+    {"SSE", "↘"},
+    {"SE", "↘"},
+    {"ESE", "↘"},
+    {"E", "→"},
+    {"ENE", "↗"},
+    {"NE", "↗"},
+    {"NNE", "↗"}
+};
+
+typedef struct {
+    const int min;
+    const int max;
+    const char *stat;
+    const char *pic;
+} Cloudy;
+
+Cloudy cloudy_stat[] = {
+    {0, 12, "Sunny", "   \\   /     \n    .-.\n ― (   ) ―   \n    `-’      \n   /   \\      \n"},
+    {13, 25, "Few clouds", "   \\  /\n__ /\"\".-.\n   \\_(   ).\n   /(__(__)\n"},
+    {26, 37, "Wisps", "    .--.\n .-(    ).\n(__)  (__)\n"},
+    {38, 50, "Scattered", "  \\  /\n_ /\"\".-.\n  \\_(  ).\n  / (__)\n"},
+    {51, 62, "Mixed", "    .--.\n .-(    ).\n(___.__)__)\n   (   )\n"},
+    {63, 75, "Partly cloudy", "  \\  /     .--.\n_ /\"\".-.  (    ).\n  \\_(   ).(__.__)\n  /(___)(__)\n"},
+    {76, 87, "Broken to overcast", "     .--.\n  .-(    ).\n (___.__)__)\n (___(___))\n"},
+    {88, 100, "Cloudy", "    .--.\n .-(    ).\n(___.__)__)\n"}
+};
+/*
+            \   /     
+             .-.      
+          ― (   ) ―   
+             `-’      
+            /   \     
+     
+     */
+/*
+      .--.     
+   .-(    ).   
+  (___.__)__)  
+               
+*/
 
 size_t write_callback(char *data, size_t size, size_t nmemb, void *userp){
     size_t total_size = size * nmemb;
@@ -31,7 +86,6 @@ size_t write_callback(char *data, size_t size, size_t nmemb, void *userp){
 
 int main(){
     struct memory chunk = {0};
-    //struct memory chunk = { .response = NULL, .size = 0 };
     char city[32];
     printf("Введите город(по английски):");
     scanf("%s", &city);
@@ -57,17 +111,62 @@ int main(){
         return 1;
     }
 
-    printf("Respone:\n%s\n", chunk.response);
+    //printf("Respone:\n%s\n", chunk.response);
 
     cJSON *json = cJSON_Parse(chunk.response);
     cJSON *current = cJSON_GetObjectItem(json, "current");
     cJSON *temp_c = cJSON_GetObjectItem(current, "temp_c");
+    cJSON *wind_kph = cJSON_GetObjectItem(current, "wind_kph");
+    cJSON *wind_dir = cJSON_GetObjectItem(current, "wind_dir");
+    cJSON *vis_km = cJSON_GetObjectItem(current, "vis_km");
+    cJSON *cloud = cJSON_GetObjectItem(current, "cloud");
 
     if(cJSON_IsNumber(temp_c)){
-        printf("Температура:%.1f\n", temp_c->valuedouble);
+        printf("%.1f°C\n", temp_c->valuedouble);
     }
     else{
-        printf("Govno");
+        fprintf(stderr, "ERROR: Unable get temperature\n");
+    }
+
+    if(cJSON_IsString(wind_dir)){
+        const int map_size = sizeof(wind_map) / sizeof(wind_map[0]);
+        for (int i = 0; i < map_size; i++) {
+            if (strcmp(wind_dir->valuestring, wind_map[i].dir) == 0) {
+                printf("%s ", wind_map[i].arrow);
+            }
+        }
+    }
+    else{
+        fprintf(stderr, "ERROr: Unable to get wind_dir\n");
+    }
+
+    if(cJSON_IsNumber(wind_kph)){
+        printf("%.1fm/s\n", (wind_kph->valuedouble * 1000) / 3600);
+    }
+    else{
+        fprintf(stderr, "ERROR: Unable to get wind_kph\n");
+        return 1; 
+    }
+
+    if(cJSON_IsNumber(vis_km)){
+        printf("%.1fkm\n", vis_km->valuedouble);
+    }
+    else{
+        fprintf(stderr, "ERROR: Unabgle to get vis\n");
+    }
+
+    if(cJSON_IsNumber(cloud)){
+        const int size = sizeof(cloudy_stat) / sizeof(cloudy_stat[0]);
+        for(int i = 0; i < size; i++){
+            if(cloud->valueint >= cloudy_stat[i].min && cloud->valueint <= cloudy_stat[i].max){
+            //if(cloud->valuedouble >= cloudy_stat[i].min && cloudy_stat[i].max >= cloud->valuedouble){
+                printf("%s\n", cloudy_stat[i].stat);
+                printf("%s\n", cloudy_stat[i].pic);
+            }
+        }
+    }
+    else{
+        fprintf(stderr, "ERROR: Unable to get cloud");
     }
 
     cJSON_Delete(json);
